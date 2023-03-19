9e657216840ff4fe770e9cc0020ef67aec6afd07
rotabey <jbaptiste.cherrier@gmail.com>
2023-03-19 17:16:12 +0100

    - pro fonct

--- mini-projet-js-202x/scripts/project202x.js
+++ mini-projet-js-202x/scripts/project202x.js
@@ -1,196 +1,187 @@
 // YOUR NAME HERE
 
-class Party {
-    constructor() {
-        this.tenthElement = this.byId('tenth');
-        this.secondsElement = this.byId('seconds');
-        this.minutesElement = this.byId('minutes');
-
-        this.nbTarget = 0;
-        this.targetList = [];
-        this.startedParty = false;
-
-        this.time = 0;
-        this.timerId = null;
-
-        this.initParty();
-    }
-
-    byId(id) {
-        if (id) {
-            return document.getElementById(id);
-        }
-    }
-
-    initParty() {
-        this.bindCreateOneTarget();
-        this.bindGenerateTarget();
-        this.byId('control').insertAdjacentHTML('beforeend', '<div id="score"></div>');
-    }
-
-    bindCreateOneTarget() {
-        this.byId('create').addEventListener('click', () => {
-            this.generateTarget(1).then(() => {
-                this.targetList[0].addEventListener('click', () => {
-                    this.targetList[0].classList.add('hit');
-                });
-            });
-        });
-    }
-
-    bindGenerateTarget() {
-        this.byId('start').addEventListener('click', () => {
-            this.prepareParty();
-        });
-    }
-
-    prepareParty() {
-        this.byId('score').textContent = '';
-        this.nbTarget = this.byId('nbtargets').value;
-
-        if (this.startedParty == true) {
-            this.startedParty = false;
-            this.displayTime(true);
-        } else {
-            if (this.time != 0) {
-                this.displayTime(true);
-            }
-        }
-
-        this.generateTarget(this.nbTarget)
-            .then(() => {
-                this.byId('remaining').textContent = this.nbTarget;
-                for (let target of this.targetList) {
-                    target.addEventListener('click', () => {
-                        if (this.startedParty == false) {
-                            this.startedParty = true;
-                            this.timer();
-                        }
-                        target.classList.add('hit');
-                        this.nbTarget -= 1;
-                        if (this.nbTarget == 0 && this.startedParty == true) {
-                            this.startedParty = false;
-                            this.displayChronoTimer();
-                        }
-                        this.byId('remaining').textContent = this.nbTarget;
-                        this.removeTarget(target);
-                    });
-                }
-            });
-    }
-
-    displayChronoTimer() {
-        let arrayTimer = this.time.split(' ');
-        let minutes = arrayTimer[0];
-        let seconds = arrayTimer[2];
-        let tenth = arrayTimer[4];
-        this.byId('score').innerHTML = "<p>Votre temps est de " + minutes + "m : " + seconds + "s . " + tenth + "</p>";
-    }
-
-    displayTime(suppr = false, minutesTime, secondsTime, tenthsecondsTime) {
-        let displaySeconds;
-        let displayMinutes;
-
-        if (suppr == true) {
-            this.tenthElement.textContent = "0";
-            this.secondsElement.textContent = "00";
-            this.minutesElement.textContent = "00";
-        } else {
-            if (tenthsecondsTime < 10) {
-                this.tenthElement.textContent = tenthsecondsTime;
-            }
-
-            if (secondsTime < 10) {
-                displaySeconds = '0' + secondsTime;
-                this.secondsElement.textContent = displaySeconds;
-            } else if (secondsTime < 60) {
-                this.secondsElement.textContent = secondsTime;
-            }
-
-            if (minutesTime < 10) {
-                displayMinutes = '0' + minutesTime;
-                this.minutesElement.textContent = displayMinutes;
-            } else {
-                this.minutesElement.textContent = minutesTime;
-            }
-        }
-    }
-
-    timer() {
-        let minutesTime = 0;
-        let secondsTime = 0;
-        let tenthsecondsTime = 0;
-
-        if (this.timerId) {
-            clearInterval(this.timerId);
-        }
-
-        this.timerId = setInterval(() => {
-            if (this.startedParty == true) {
-                tenthsecondsTime += 1;
-                if (tenthsecondsTime == 10) {
-                    tenthsecondsTime = 0;
-                    secondsTime += 1;
-                }
-                if (secondsTime == 60) {
-                    secondsTime = 0;
-                    minutesTime += 1;
-                }
-                this.time = `${minutesTime}` + ' : ' + `${secondsTime}` + ' . ' + `${tenthsecondsTime}`;
-                this.displayTime(false, minutesTime, secondsTime, tenthsecondsTime);
-            }
-        }, 100);
-    }
-
-    async generateTarget(n = 0) {
-        let level = this.byId('level').value;
-        console.log(level);
-        let terrain = this.byId('terrain');
-        let emptyPlayground = true;
-
-        if (this.targetList.length > 0) {
-            emptyPlayground = await this.removeAllTarget();
-            this.targetList = [];
-        }
-
-        if (emptyPlayground) {
-            for (let i = 0; i < n; i++) {
-                let target = document.createElement('div');
-                target.classList = 'target';
-                if (level == 'hard') {
-                    this.byId('terrain').classList.add('hard-playground');
-                    target.classList.add('hard');
-                } else {
-                    this.byId('terrain').classList.remove('hard-playground');
-                    target.classList.remove('hard');
-                }
-                target.classList.add('on');
-                let position_left = 350;
-                if (level == 'hard'){
-                    position_left = 550;
-                }
-                target.style.left = Math.floor(Math.random() * position_left) + 'px';
-                target.style.top = Math.floor(Math.random() * 350) + 'px';
-                terrain.appendChild(target);
-                this.targetList.push(target);
-            }
-        }
-    }
-
-    removeTarget(target) {
-        setTimeout(() => {
-            this.byId('terrain').removeChild(target);
-        }, 1000);
-    }
-
-    removeAllTarget() {
-        return new Promise((resolve) => {
-            for (let target of this.targetList) {
-                target.remove();
-            }
-            resolve(true);
-        });
+function byId(id) {
+    if (id) {
+        return document.getElementById(id);
     }
 }
 
-new Party();
+const tenthElement = byId('tenth');
+const secondsElement = byId('seconds');
+const minutesElement = byId('minutes');
+
+let nbTarget = 0;
+let targetList = [];
+let startedParty = false;
+
+let time = 0;
+let timerId = null;
+
+function initParty() {
+    bindCreateOneTarget();
+    bindGenerateTarget();
+    byId('control').insertAdjacentHTML('beforeend', '<div id="score"></div>');
+}
+
+function bindCreateOneTarget() {
+    byId('create').addEventListener('click', () => {
+        generateTarget(1).then(() => {
+            targetList[0].addEventListener('click', () => {
+                targetList[0].classList.add('hit');
+            });
+        });
+    });
+}
+
+function bindGenerateTarget() {
+    byId('start').addEventListener('click', () => {
+        prepareParty();
+    });
+}
+
+function prepareParty() {
+    byId('score').textContent = '';
+    nbTarget = byId('nbtargets').value;
+    if (startedParty == true) {
+        startedParty = false;
+        displayTime(true);
+    } else {
+        if (time != 0) {
+            displayTime(true);
+        }
+    }
+
+    generateTarget(nbTarget)
+        .then(() => {
+            byId('remaining').textContent = nbTarget;
+            for (let target of targetList) {
+                target.addEventListener('click', () => {
+                    if (startedParty == false) {
+                        startedParty = true;
+                        timer();
+                    }
+                    target.classList.add('hit');
+                    nbTarget -= 1;
+                    if (nbTarget == 0 && startedParty == true) {
+                        startedParty = false;
+                        displayChronoTimer();
+                    }
+                    byId('remaining').textContent = nbTarget;
+                    removeTarget(target);
+                });
+            }
+        });
+}
+
+async function generateTarget(n = 0) {
+    let level = byId('level').value;
+    let terrain = byId('terrain');
+    let emptyPlayground = true;
+
+    if (targetList.length > 0) {
+        emptyPlayground = await removeAllTarget();
+        targetList = [];
+    }
+
+    if (emptyPlayground) {
+        for (let i = 0; i < n; i++) {
+            let target = document.createElement('div');
+            target.classList = 'target';
+            if (level == 'hard') {
+                byId('terrain').classList.add('hard-playground');
+                target.classList.add('hard');
+            } else {
+                byId('terrain').classList.remove('hard-playground');
+                target.classList.remove('hard');
+            }
+            target.classList.add('on');
+            let position_left = 350;
+            if (level == 'hard') {
+                position_left = 550;
+            }
+            target.style.left = Math.floor(Math.random() * position_left) + 'px';
+            target.style.top = Math.floor(Math.random() * 350) + 'px';
+            terrain.appendChild(target);
+            targetList.push(target);
+        }
+    }
+}
+
+function removeTarget(target) {
+    setTimeout(() => {
+        byId('terrain').removeChild(target);
+    }, 1000);
+}
+
+function removeAllTarget() {
+    return new Promise((resolve) => {
+        for (let target of targetList) {
+            target.remove();
+        }
+        resolve(true);
+    });
+}
+
+function displayTime(suppr = false, minutesTime, secondsTime, tenthsecondsTime) {
+    let displaySeconds;
+    let displayMinutes;
+
+    if (suppr == true) {
+        tenthElement.textContent = "0";
+        secondsElement.textContent = "00";
+        minutesElement.textContent = "00";
+    } else {
+        if (tenthsecondsTime < 10) {
+            tenthElement.textContent = tenthsecondsTime;
+        }
+
+        if (secondsTime < 10) {
+            displaySeconds = '0' + secondsTime;
+            secondsElement.textContent = displaySeconds;
+        } else if (secondsTime < 60) {
+            secondsElement.textContent = secondsTime;
+        }
+
+        if (minutesTime < 10) {
+            displayMinutes = '0' + minutesTime;
+            minutesElement.textContent = displayMinutes;
+        } else {
+            minutesElement.textContent = minutesTime;
+        }
+    }
+}
+
+function displayChronoTimer() {
+    byId('score').innerHTML = "<p>Votre temps est de " + minutesElement.textContent + "m : " + secondsElement.textContent + "s . " + tenthElement.textContent + "</p>";
+}
+
+function timer() {
+    let minutesTime = 0;
+    let secondsTime = 0;
+    let tenthsecondsTime = 0;
+
+    if (timerId) {
+        clearInterval(timerId);
+    }
+
+    timerId = setInterval(() => {
+        if (startedParty == true) {
+            tenthsecondsTime = time / 100;
+            if (tenthsecondsTime == 10) {
+                tenthsecondsTime = 0;
+            }
+            if (time == 1000) {
+                time = 0;
+                secondsTime += 1;
+            }
+            if (secondsTime == 60) {
+                secondsTime = 0;
+                minutesTime += 1;
+            }
+            displayTime(false, minutesTime, secondsTime, tenthsecondsTime);
+            time += 100;
+        }
+    }, 100);
+}
+
+initParty();
--- mini-projet-ocaml-202x/backtracking.ml
+++ mini-projet-ocaml-202x/backtracking.ml
@@ -1,72 +1,73 @@
-let absentSurLigne k g i =
-  let present = ref false and j = ref 0 in
-  while not !present
-        && !j < Array.length g.(i) do
-    if g.(i).(!j) = k
-    then present := true;
-    incr j;
-  done;
-  not !present;;
-
-let absentSurCol k g j =
-  try
-    for i = 0 to (Array.length g -1) do
-      if g.(i).(j) = k
-      then raise Exit;
-    done;
-    true
-  with Exit -> false;; 
-
-let absentSurBloc k g i j =
-  let n2 = Array.length g in
+let absent_sur_block k_number grid line colonne =
+  let n2 = Array.length grid in
   let n =
     int_of_float (sqrt (float_of_int n2))
-  in let u = i - i mod n and
-      v = j - j mod n in
+  in let u = line - line mod n and
+      v = colonne - colonne mod n in
   try
     for u' = u to u+n-1 do
       for v' = v to v+n-1 do
-        if g.(u').(v')=k then raise Exit;
+        if grid.(u').(v')=k_number then raise Exit;
       done;
     done;  true;
    with Exit -> false;;
 
+let absent_sur_ligne k_number grid line =
+  let present = ref false and colonne = ref 0 in
+  while not !present
+        && !colonne < Array.length grid.(line) do
+    if grid.(line).(!colonne) = k_number
+    then present := true;
+    incr colonne;
+  done;
+  not !present;;
+
+let absent_sur_colonne k_number grid colonne =
+  try
+    for line = 0 to (Array.length grid -1) do
+      if grid.(line).(colonne) = k_number
+      then raise Exit;
+    done;
+    true
+  with Exit -> false;;
+
+
 exception Trouve;;
 
-let remplir g =
-  let nbl = Array.length g in
+let remplir grid =
+  let nbl = Array.length grid in
   let rec aux p =
     if p = nbl*nbl then true
     else
       begin
-        let i = p/nbl and j = p mod nbl in
-        if g.(i).(j) <> 0 then aux (p+1)
+        let line = p/nbl and colonne = p mod nbl in
+        if grid.(line).(colonne) <> 0 then aux (p+1)
         else
           (
-            let k = ref 1 in
+            let k_number = ref 1 in
             try
-              while !k <=nbl do
-                if absentSurLigne !k g i
-                   && absentSurCol !k g j
-                   && absentSurBloc !k g i j then
+              while !k_number <=nbl do
+                if absent_sur_ligne !k_number grid line
+                   && absent_sur_colonne !k_number grid colonne
+                   && absent_sur_block !k_number grid line colonne then
                   begin
-                    g.(i).(j) <- !k;(*occuper; pas en avant*)
+                    grid.(line).(colonne) <- !k_number;
                     if aux (p+1) then raise Trouve;
-                  end;              
-                incr k;
+                  end;
+                incr k_number;
               done;
-              g.(i).(j)<-0;(*libérer; pas en arrière*)
+              grid.(line).(colonne)<-0;(*libérer; pas en arrière*)
               false
             with Trouve -> true
           )
       end in
   aux 0;;
 
-let print_grille matrix = 
+let display_grid matrix =
   for y = 0 to 8 do
     print_string "| ";
     for x = 0 to 8 do
-      
+
       print_int matrix.(y).(x);
       print_string " | ";
     done;
@@ -75,7 +76,7 @@ let print_grille matrix =
       else print_string "\n";
   done;;
 
-let grille =    [|
+let sudoku_grid =    [|
   [|9;0;0;1;0;0;0;0;5|];
   [|0;0;5;0;9;0;2;0;1|];
   [|8;0;0;0;4;0;0;0;0|];
@@ -87,20 +88,19 @@ let grille =    [|
   [|0;0;1;9;0;4;5;7;0|]
 |];;
 
-absentSurLigne 3 grille 0;;
-absentSurLigne 1 grille 0;;
-absentSurCol 3 grille 0;;
-absentSurCol 2 grille 0;;
-absentSurBloc 8 grille 5 4;;
-absentSurBloc 4 grille 2 2;;
+absent_sur_colonne 3 sudoku_grid 0;;
+absent_sur_colonne 2 sudoku_grid 0;;
 
-print_grille grille;;
+absent_sur_ligne 3 sudoku_grid 0;;
+absent_sur_ligne 1 sudoku_grid 0;;
 
-remplir grille;;
+absent_sur_block 8 sudoku_grid 5 4;;
+absent_sur_block 4 sudoku_grid 2 2;;
 
-grille;;
+display_grid sudoku_grid;;
 
-print_grille grille;;
+remplir sudoku_grid;;
 
+sudoku_grid;;
 
-
+display_grid sudoku_grid;;
